ce9f29fb69e269e84ef871bd7f2147d46b9f2743
Zykaz <64371088+Zykaz@users.noreply.github.com>
2023-06-05 11:56:01 +0800

    Upload

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# 使用官方的Node.js映像作為基礎
+FROM node:14
+
+# 設定工作目錄
+WORKDIR /app
+
+# 複製專案檔案到容器中
+COPY package.json package-lock.json /app/
+
+# 安裝相依套件
+RUN npm install
+
+# 複製應用程式代碼到容器中
+COPY . /app
+
+# 定義容器的執行命令
+CMD ["node", "app.js"]
---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// 使用Session中間件
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// 使用Body Parser中間件解析表單資料
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 登入頁面路由
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>登入</h1>
+    <form method="POST" action="/login">
+      <label for="username">使用者名稱:</label>
+      <input type="text" id="username" name="username" required><br>
+      <label for="password">密碼:</label>
+      <input type="password" id="password" name="password" required><br>
+      <button type="submit">登入</button>
+    </form>
+  `);
+});
+
+// 登入請求處理
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username && password) {
+    // 設定登入成功的Session變數
+    req.session.loggedIn = true;
+    req.session.username = username;
+    res.redirect('/dashboard');
+  } else {
+    res.send('無效的使用者名稱或密碼');
+  }
+});
+
+// 登入後的頁面
+app.get('/dashboard', (req, res) => {
+  // 檢查使用者是否已登入
+  if (req.session.loggedIn) {
+    res.send(`
+      <h1>歡迎, ${req.session.username}!</h1>
+      <p>這是你的個人儀表板。</p>
+      <a href="/logout">登出</a>
+    `);
+  } else {
+    res.redirect('/');
+  }
+});
+
+// 登出處理
+app.get('/logout', (req, res) => {
+  // 清除Session變數並導向登入頁面
+  req.session.destroy();
+  res.redirect('/');
+});
+
+// 監聽3000埠
+app.listen(3000, () => {
+  console.log('應用程式已在 http://localhost:3000 上運行');
+});
